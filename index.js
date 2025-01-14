702ef34f4ed4747ec5fe2c02849a65c1426796b5
shannuboda <shanmukha.boda@gmail.com>
2025-01-14 11:02:37 +0530

    first

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const db1 = mongoose.createConnection(
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
-db1.on("open", () => console.log("Connected to Database 1"));
+db1.on("open", () => console.log("Connected to Database Users"));
 db1.on("error", (err) => console.error("Error connecting to Database 1:", err));
 
 // MongoDB connection for the second database
@@ -25,8 +25,30 @@ const db2 = mongoose.createConnection(
     { useNewUrlParser: true, useUnifiedTopology: true }
   );
   
-  db2.on("open", () => console.log("Connected to Database 2"));
-  db2.on("error", (err) => console.error("Error connecting to Database 2:", err));
+  db2.on("open", () => console.log("Connected to Database Student File"));
+  db2.on("error", (err) => console.error("Error connecting to Database Student File:", err));
+  
+
+// Connection for Semester1 database
+
+const db3 = mongoose.createConnection(
+    "mongodb+srv://bssmani16:9z7gYDVx2XNAB3qG@cluster0.oeyyh.mongodb.net/SEMESTER1?retryWrites=true&w=majority", 
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+  
+  db3.on("open", () => console.log("Connected to Database for Semester 1"));
+  db3.on("error", (err) => console.error("Error connecting to Database Student File:", err));
+  
+
+// Connection for Semester2 database
+
+const db4 = mongoose.createConnection(
+    "mongodb+srv://bssmani16:9z7gYDVx2XNAB3qG@cluster0.oeyyh.mongodb.net/SEMESTER2?retryWrites=true&w=majority", 
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+  
+  db4.on("open", () => console.log("Connected to Database for Semester 2"));
+  db4.on("error", (err) => console.error("Error connecting to Database Student File:", err));
   
 
 // Example route for interacting with the second database
@@ -51,6 +73,52 @@ app.get('/db1/students', async (req, res) => {
       console.error('Error fetching data from SvPortalusers database:', err);
       res.status(500).json({ message: 'Error fetching data', error: err });
     }
+  });
+
+
+
+//   sem 1 data start API
+
+app.get('/getsem1/:id/:id2/:id3', async (req, res) => {
+    const rno = req.params.id
+    const year = req.params.id2
+    const semester = req.params.id3
+    console.log(rno,year,semester);
+    // for sem1
+    if(semester==="SEMESTER1")
+    {
+        try {
+            const students = await db3.db.collection(year).find({"rollno":rno}).toArray();
+            if(!students.length>0)
+            {
+              res.status(200).json({ message: 'No Data Found' });
+            }
+            else{
+              res.json(students);
+            }
+           
+          } catch (err) {
+            console.error('Error fetching data from SvPortalusers database:', err);
+            res.status(500).json({ message: 'Error fetching data', error: err });
+          }
+    }
+    else if(semester==="SEMESTER2"){
+        try {
+            const students = await db4.db.collection(year).find({"rollno":rno}).toArray();
+            if(!students.length>0)
+            {
+              res.status(200).json({ message: 'No Data Found' });
+            }
+            else{
+              res.json(students);
+            }
+           
+          } catch (err) {
+            console.error('Error fetching data from SvPortalusers database:', err);
+            res.status(500).json({ message: 'Error fetching data', error: err });
+          }
+    }
+   
   });
 
 
